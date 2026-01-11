3cc0a4d05c456489840e5c9dfce2339ac5b45be0
Peter Zeller <p_zeller@cs.uni-kl.de>
2026-01-11 14:03:36 +0100

    rewrite extended lexer to scala3

--- src/main/scala/com/github/peterzeller/minifumo/lexer/ExtendedLexer.scala
+++ src/main/scala/com/github/peterzeller/minifumo/lexer/ExtendedLexer.scala
@@ -4,7 +4,6 @@ import org.antlr.v4.runtime._
 import org.antlr.v4.runtime.misc.Pair
 import java.util
 
-
 import com.github.peterzeller.minifumo.antlr.MinifumoLexer
 import com.github.peterzeller.minifumo.antlr.MinifumoParser
 import org.antlr.v4.runtime.atn.ATNConfigSet
@@ -15,35 +14,26 @@ import scala.collection.mutable
 import scala.jdk.CollectionConverters.ListHasAsScala
 import com.github.peterzeller.minifumo.lexer.ExtendedLexer.ExtendedMinifumoLexer.State
 
-object ExtendedLexer {
+object ExtendedLexer:
 
-  object WLogger {
-    def info(str: String): Unit = {
+  object WLogger:
+    def info(str: String): Unit =
       println(str)
-    }
 
-    def trace(str: String): Unit = {
+    def trace(str: String): Unit =
       println(str)
-    }
-  }
 
-  object ExtendedMinifumoLexer {
+  object ExtendedMinifumoLexer:
+    enum State:
+      case INIT, NEWLINES, BEGIN_LINE
 
-    object State extends Enumeration {
-      type State = Value
-      val INIT, NEWLINES, BEGIN_LINE = Value
-    }
-
-
-  }
-
-  class ExtendedMinifumoLexer(val input: CharStream) extends TokenSource {
+  class ExtendedMinifumoLexer(val input: CharStream) extends TokenSource:
     final private val orig: MinifumoLexer = new MinifumoLexer(input)
     private val sourcePair: Pair[TokenSource, CharStream] = new Pair[TokenSource, CharStream](orig, input)
     private val indentationLevels: mutable.Stack[Integer] = new mutable.Stack[Integer]
     indentationLevels.push(0)
     private val nextTokens: util.Queue[Token] = new util.LinkedList[Token]
-    private var state: State.State = ExtendedMinifumoLexer.State.INIT
+    private var state: State = ExtendedMinifumoLexer.State.INIT
 
     private var spacesPerIndent: Int = -(1)
     private var eof: Option[Token] = None
@@ -57,267 +47,209 @@ object ExtendedLexer {
     // counts the number of open parentheses
     private var parenthesesLevel: Int = 0
 
-    override def getCharPositionInLine: Int = {
+    override def getCharPositionInLine: Int =
       orig.getCharPositionInLine
-    }
 
-    override def getInputStream: CharStream = {
+    override def getInputStream: CharStream =
       orig.getInputStream
-    }
 
-    override def getLine: Int = {
+    override def getLine: Int =
       orig.getLine
-    }
 
-    override def getSourceName: String = {
+    override def getSourceName: String =
       orig.getSourceName
-    }
 
-    override def getTokenFactory: TokenFactory[_] = {
+    override def getTokenFactory: TokenFactory[?] =
       orig.getTokenFactory
-    }
 
-    override def nextToken: Token = {
+    override def nextToken: Token =
       val t: Token = nextTokenIntern
       lastToken = Some(t)
-      if (debug) {
-        WLogger.trace("     new token  = " + MinifumoParser.VOCABULARY.getSymbolicName(t.getType) + " '" + t.getText + "'")
-      }
+      if debug then
+        WLogger.trace(
+          "     new token  = " + MinifumoParser.VOCABULARY.getSymbolicName(t.getType) + " '" + t.getText + "'"
+        )
       t
-    }
 
     def addErrorListener(errorListener: ANTLRErrorListener): Unit =
       orig.addErrorListener(errorListener)
 
-    private def nextTokenIntern: Token = {
-      if (!(nextTokens.isEmpty)) {
+    private def nextTokenIntern: Token =
+      if !nextTokens.isEmpty then
         return nextTokens.poll
-      }
       val l_eof: Option[Token] = eof
-      for (eof <- l_eof) {
+      for eof <- l_eof do
         return makeToken(Token.EOF, "$EOF", eof.getStartIndex, eof.getStopIndex)
-      }
 
-      //      @tailrec
-      def continue(): Token = {
+      def continue(): Token =
         val token1: Token = orig.nextToken
-        if (debug) {
-          WLogger.info(s"$state orig token = " + MinifumoParser.VOCABULARY.getSymbolicName(token1.getType) + " '" + token1.getText + "'")
-        }
-        if (token1 == null) {
+        if debug then
+          WLogger.info(
+            s"$state orig token = " + MinifumoParser.VOCABULARY.getSymbolicName(token1.getType) + " '" + token1.getText + "'"
+          )
+        if token1 == null then
           return null
-        }
         val token: Token = token1
-        if (token.getType == MinifumoParser.NL) {
+        if token.getType == MinifumoParser.NL then
           var line: Int = 0
-          for (i <- 0 until token.getText.length) {
+          for i <- 0 until token.getText.length do
             val c: Char = token.getText.charAt(i)
-            if (c == '\n') {
+            if c == '\n' then
               line += 1
-            }
-          }
-        } else {
-          if (token.getType == MinifumoParser.PAREN_LEFT) {
+        else
+          if token.getType == MinifumoParser.PAREN_LEFT then
             parenthesesLevel += 1
-          } else if (token.getType == MinifumoParser.PAREN_RIGHT) {
+          else if token.getType == MinifumoParser.PAREN_RIGHT then
             parenthesesLevel -= 1
-          } else if (token.getType == Token.EOF) { // at EOF close all blocks and return an extra newline
+          else if token.getType == Token.EOF then
             handleIndent(0, token, token.getStartIndex, token.getStopIndex, Some(token))
             eof = Some(token)
             // if inside Minifumo, add a closing newline
             nextTokens.add(makeToken(MinifumoParser.NL, "$NL", token.getStartIndex, token.getStopIndex))
             // add a single newline
             return makeToken(MinifumoParser.NL, "$NL", token.getStartIndex, token.getStopIndex)
-          }
-        }
-        state match {
+        state match
           case State.INIT =>
-            if (token.getType == MinifumoParser.NL) {
-              if (lastCharWasWrap) { // ignore the newline following a wrap-character
+            if token.getType == MinifumoParser.NL then
+              if lastCharWasWrap then
                 return continue()
-              } else {
+              else
                 firstNewline = Some(token)
                 state(ExtendedMinifumoLexer.State.NEWLINES)
                 return continue()
-              }
-            } else if (isTab(token)) {
+            else if isTab(token) then
               return continue()
-            }
             lastCharWasWrap = isWrapCharEndLine(token.getType)
             return token
           case State.NEWLINES =>
-            if (isWrapCharBeginLine(token.getType)) {
+            if isWrapCharBeginLine(token.getType) then
               // ignore all the newlines when a wrap char comes after newlines
               lastCharWasWrap = isWrapChar(token.getType)
               state(ExtendedMinifumoLexer.State.INIT)
               return token
-            } else if (token.getType == MinifumoParser.NL) {
-              return continue() //todo: continue is not supported
-            }
-            else if (isTab(token)) {
+            else if token.getType == MinifumoParser.NL then
+              return continue()
+            else if isTab(token) then
               state(ExtendedMinifumoLexer.State.BEGIN_LINE)
               numberOfTabs = tabWidth(token)
-              return continue() //todo: continue is not supported
-
-            } else { // no tabs after newline
+              return continue()
+            else
               handleIndent(0, token, token.getStartIndex, token.getStopIndex, firstNewline)
               nextTokens.add(token)
               state(ExtendedMinifumoLexer.State.INIT)
               return firstNewline.get
-            }
           case State.BEGIN_LINE =>
-            if (isTab(token)) {
+            if isTab(token) then
               numberOfTabs += tabWidth(token)
-            }
-            else {
-              if (token.getType == MinifumoParser.NL) {
+            else
+              if token.getType == MinifumoParser.NL then
                 state(ExtendedMinifumoLexer.State.NEWLINES)
-              }
-              else {
-                if (isWrapCharBeginLine(token.getType)) {
-                  lastCharWasWrap = isWrapChar(token.getType)
-                  state(ExtendedMinifumoLexer.State.INIT)
-                  return token
-                }
-                else {
-                  if (lastCharWasWrap && numberOfTabs > indentationLevels.top) { // ignore the newline, only return the token
-                    state(ExtendedMinifumoLexer.State.INIT)
-                    return token
-                  }
-                  else {
-                    handleIndent(numberOfTabs, token, token.getStartIndex, token.getStopIndex, firstNewline)
-                    state(ExtendedMinifumoLexer.State.INIT)
-                    nextTokens.add(token)
-                    return firstNewline.get
-                  }
-                }
-              }
-            }
-        }
+              else if isWrapCharBeginLine(token.getType) then
+                lastCharWasWrap = isWrapChar(token.getType)
+                state(ExtendedMinifumoLexer.State.INIT)
+                return token
+              else if lastCharWasWrap && numberOfTabs > indentationLevels.top then
+                state(ExtendedMinifumoLexer.State.INIT)
+                return token
+              else
+                handleIndent(numberOfTabs, token, token.getStartIndex, token.getStopIndex, firstNewline)
+                state(ExtendedMinifumoLexer.State.INIT)
+                nextTokens.add(token)
+                return firstNewline.get
         continue()
-      }
 
       continue()
-    }
 
-    def tabWidth(token: Token): Int = {
+    def tabWidth(token: Token): Int =
       val len: Int = 1 + token.getStopIndex - token.getStartIndex
-      token.getType match {
+      token.getType match
         case MinifumoParser.SPACETAB =>
           len
         case _ =>
           throw new IllegalArgumentException
-      }
-    }
 
-
-    private def isTab(token: Token): Boolean = {
+    private def isTab(token: Token): Boolean =
       token.getType == MinifumoParser.SPACETAB
-    }
 
-
-    private def state(s: ExtendedMinifumoLexer.State.State): Unit = {
-      if (debug) {
+    private def state(s: ExtendedMinifumoLexer.State): Unit =
+      if debug then
         WLogger.info("state " + state + " -> " + s)
-      }
       state = s
-    }
 
-    private def handleIndent(n: Int, token: Token, start: Int, stop: Int, endBlockToken: Option[Token]): Unit = {
-      if (debug) {
-        WLogger.info("handleIndent " + n + "	 " + indentationLevels)
-      }
-      if (n > indentationLevels.top) {
-        if (spacesPerIndent < 0) {
+    private def handleIndent(n: Int, token: Token, start: Int, stop: Int, endBlockToken: Option[Token]): Unit =
+      if debug then
+        WLogger.info("handleIndent " + n + "\t " + indentationLevels)
+      if n > indentationLevels.top then
+        if spacesPerIndent < 0 then
           spacesPerIndent = n
-        }
         indentationLevels.push(n)
         nextTokens.add(makeToken(MinifumoParser.BEGIN, "$begin", start, stop))
-      }
-      else {
-        while ( {
-          n < indentationLevels.top
-        }) {
+      else
+        while n < indentationLevels.top do
           indentationLevels.pop
-          nextTokens.add(makeToken(MinifumoParser.END, "$end",
-            endBlockToken.map(_.getStartIndex).getOrElse(0),
-            endBlockToken.map(_.getStartIndex).getOrElse(0)))
-        }
+          nextTokens.add(
+            makeToken(
+              MinifumoParser.END,
+              "$end",
+              endBlockToken.map(_.getStartIndex).getOrElse(0),
+              endBlockToken.map(_.getStartIndex).getOrElse(0)
+            )
+          )
         val expectedIndentation: Integer = indentationLevels.top
-        if (n != expectedIndentation) { // all lines must be indented on the same level,
-          // with the exception of the 'end' keyword, which can be on a different line
-          val msg: String = "Invalid indentation level. Current indentation is " + expectedIndentation + ", but this is indented by " + n + "."
-          for (el <- orig.getErrorListeners.asScala) {
+        if n != expectedIndentation then
+          val msg: String =
+            "Invalid indentation level. Current indentation is " + expectedIndentation + ", but this is indented by " + n + "."
+          for el <- orig.getErrorListeners.asScala do
             val line = token.getLine
             el.syntaxError(orig, "", line, token.getCharPositionInLine, msg, null)
-          }
-        }
-      }
-    }
 
-    private def isWrapChar(tokenKind: Int): Boolean = {
-      tokenKind match {
+    private def isWrapChar(tokenKind: Int): Boolean =
+      tokenKind match
         case MinifumoParser.COMMA
-             | MinifumoParser.PLUS
-             | MinifumoParser.MULT
-             | MinifumoParser.MINUS
-             | MinifumoParser.DIV
-             | MinifumoParser.MOD
-             | MinifumoParser.AND
-             | MinifumoParser.OR
-             | MinifumoParser.COLON
-             | MinifumoParser.COLONCOLON
-             | MinifumoParser.EQ
-             | MinifumoParser.EQEQ
-             | MinifumoParser.NOTEQ
-             | MinifumoParser.BAR
-             | MinifumoParser.IMPLIES
-             | MinifumoParser.IFF => true
+            | MinifumoParser.PLUS
+            | MinifumoParser.MULT
+            | MinifumoParser.MINUS
+            | MinifumoParser.DIV
+            | MinifumoParser.MOD
+            | MinifumoParser.AND
+            | MinifumoParser.OR
+            | MinifumoParser.COLON
+            | MinifumoParser.COLONCOLON
+            | MinifumoParser.EQ
+            | MinifumoParser.EQEQ
+            | MinifumoParser.NOTEQ
+            | MinifumoParser.BAR
+            | MinifumoParser.IMPLIES
+            | MinifumoParser.IFF => true
         case _ => false
-      }
-    }
 
-    private def isWrapCharEndLine(tokenKind: Int): Boolean = {
-      tokenKind match {
+    private def isWrapCharEndLine(tokenKind: Int): Boolean =
+      tokenKind match
         case MinifumoParser.PAREN_LEFT | MinifumoParser.BRACKET_LEFT | MinifumoParser.BRACE_LEFT =>
           true
         case _ =>
           isWrapChar(tokenKind)
-      }
-    }
 
-    private def isWrapCharBeginLine(`type`: Int): Boolean = {
-      `type` match {
+    private def isWrapCharBeginLine(`type`: Int): Boolean =
+      `type` match
         case MinifumoParser.PAREN_RIGHT
-             | MinifumoParser.BRACKET_RIGHT
-             | MinifumoParser.BRACE_RIGHT
-             | MinifumoParser.NOT
-             | MinifumoParser.DOT =>
+            | MinifumoParser.BRACKET_RIGHT
+            | MinifumoParser.BRACE_RIGHT
+            | MinifumoParser.NOT
+            | MinifumoParser.DOT =>
           true
         case _ =>
           isWrapChar(`type`)
-      }
-    }
 
-    private def makeToken(`type`: Int, text: String, start: Int, stop: Int): Token = {
+    private def makeToken(`type`: Int, text: String, start: Int, stop: Int): Token =
       val source: Pair[TokenSource, CharStream] = sourcePair
       val channel: Int = 0
       val t: CommonToken = new CommonToken(source, `type`, channel, start, stop)
       t
-    }
 
-    override def setTokenFactory(factory: TokenFactory[_]): Unit = {
+    override def setTokenFactory(factory: TokenFactory[?]): Unit =
       orig.setTokenFactory(factory)
-    }
 
-
-    def setErrorListener(listener: ANTLRErrorListener): Unit = {
+    def setErrorListener(listener: ANTLRErrorListener): Unit =
       orig.removeErrorListeners()
       orig.addErrorListener(listener)
-    }
-
-
-  }
-
-
-}
