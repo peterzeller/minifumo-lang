809c82438b4dd9fa577f349f62bcc8127b202384
Peter Zeller <p_zeller@cs.uni-kl.de>
2026-01-12 21:57:26 +0100

    ast

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,6 +1,7 @@
 // For more information on writing tests, see
 import com.github.peterzeller.minifumo.parser.parseInput
 import com.github.peterzeller.minifumo.antlr.MinifumoParser
+import com.github.peterzeller.minifumo.ast.AstTransform
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
@@ -8,6 +9,8 @@ class MySuite extends munit.FunSuite {
       |fun add(x Int, y Int) Int
       |    x + y
     """.stripMargin)
-    println(c.toStringTree(new MinifumoParser(null)))
+    println("CST: " + c.toStringTree(new MinifumoParser(null)))
+    val ast = AstTransform.program(c)
+    println("AST: " + ast)
   }
 }
