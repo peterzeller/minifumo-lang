e8ecd55808a3fbc6acac1a4eab94377309860314
Peter Zeller <p_zeller@cs.uni-kl.de>
2026-01-12 21:37:24 +0100

    first parser test pass

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,13 @@
 // For more information on writing tests, see
+import com.github.peterzeller.minifumo.parser.parseInput
+import com.github.peterzeller.minifumo.antlr.MinifumoParser
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+    val c = parseInput("""
+      |fun add(x Int, y Int) Int
+      |    x + y
+    """.stripMargin)
+    println(c.toStringTree(new MinifumoParser(null)))
   }
 }
