28b3795d66936a7a8b0336cf065a51fa90558698
Peter Zeller <p_zeller@cs.uni-kl.de>
2026-03-09 22:44:59 +0100

    Propagate expected types through let-checking

--- src/main/scala/com/github/peterzeller/minifumo/typing/CheckLetExpr.scala
+++ src/main/scala/com/github/peterzeller/minifumo/typing/CheckLetExpr.scala
@@ -20,6 +20,21 @@ object CheckLetExpr:
     val (bodyExpr, bodyType, errs2) = TypeChecker.infer(body)(using bodyCtx, metas, ids)
     (TypedAst.Expr.LetIn(symbol, isConstant = false, valueType, valueExpr, bodyExpr)(expr.source), bodyType, errs ++ errs2)
 
+  /** Checks a let-in expression against an expected body type. */
+  def check(expr: ast.Expr.LetIn, expectedType: TypedAst.Expr)(using ctx: TypeContext, metas: MetaContext, ids: IdSupply): (TypedAst.Expr, List[TypeError]) =
+    val ast.Expr.LetIn(name, declaredType, value, body) = expr
+    val inferredValue = declaredType match
+      case Some(tpeExpr) =>
+        val expected = signatureExpr(tpeExpr, ctx.globals, localSymbols(ctx))
+        val (typedValue, errs) = TypeChecker.check(value, expected)
+        (typedValue, expected, errs)
+      case None => TypeChecker.inferAndElaborate(value)
+    val (valueExpr, valueType, errs) = inferredValue
+    val symbol = LocalSymbol(name, valueType, ids.freshLocalId())
+    val bodyCtx = ctx.withLocal(symbol, Some(valueExpr))
+    val (bodyExpr, bodyErrs) = TypeChecker.check(body, expectedType)(using bodyCtx, metas, ids)
+    (TypedAst.Expr.LetIn(symbol, isConstant = false, valueType, valueExpr, bodyExpr)(expr.source), errs ++ bodyErrs)
+
 
   /** Collects locally bound symbols that may appear in typed let annotations. */
   private def localSymbols(ctx: TypeContext): Map[String, TermSymbol] =
